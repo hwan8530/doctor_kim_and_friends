f119c91c80c81aa1de99fbc7d9901b24c28a1234
LeeJiHyeong <mky1168@naver.com>
2020-01-09 19:58:55 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>권오범 짱짱 잘생김</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
